a2bf7f6be24ae34de5f951f75af1c9196edf044a
Pethum Jeewantha <me@pethumjeewantha.com>
2024-04-13 08:31:55 +0530

    Change redirect url

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,7 +1,7 @@
 import {envVariables} from "../lib/http.lib.ts";
 
 export const asgardeoConfigs =  {
-    signInRedirectURL: `${envVariables.url}`,
+    signInRedirectURL: `${envVariables.url}/dashboard`,
     clientID: envVariables.asgardeoClientId,
     baseUrl: "https://api.asgardeo.io/t/pethumjeewantha",
     scope: [ "openid","profile" ]
